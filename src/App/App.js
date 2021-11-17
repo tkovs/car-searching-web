365890725f845c9f80cbeb26c915a93e8662f24a
VictorLucenna <andvtor@gmail.com>
2021-11-17 14:31:28 -0300

    Simpliciando Cards

--- src/App/App.js
+++ src/App/App.js
@@ -1,3 +1,4 @@
+import Card from "../Card";
 import Paragraph from "../Paragraph";
 import "../styles.css";
 
@@ -10,124 +11,28 @@ const App = () => (
     </div>
     <div className="container">
       <div className="row">
-        <div className="three columns">
-          <img
-            className="card"
-            alt="alt"
-            src="https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"
-          />
-        </div>
-        <div className="three columns">
-          <img
-            className="card"
-            alt="alt"
-            src="https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"
-          />
-        </div>
-        <div className="three columns">
-          <img
-            className="card"
-            alt="alt"
-            src="https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"
-          />
-        </div>
-        <div className="three columns">
-          <img
-            className="card"
-            alt="alt"
-            src="https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"
-          />
-        </div>
+        <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
+        <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
+        <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
+        <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
       </div>
       <div className="row">
-        <div className="three columns">
-          <img
-            className="card"
-            alt="alt"
-            src="https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"
-          />
-        </div>
-        <div className="three columns">
-          <img
-            className="card"
-            alt="alt"
-            src="https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"
-          />
-        </div>
-        <div className="three columns">
-          <img
-            className="card"
-            alt="alt"
-            src="https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"
-          />
-        </div>
-        <div className="three columns">
-          <img
-            className="card"
-            alt="alt"
-            src="https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"
-          />
-        </div>
+        <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
+        <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
+        <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
+        <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
       </div>
       <div className="row">
-        <div className="three columns">
-          <img
-            className="card"
-            alt="alt"
-            src="https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"
-          />
-        </div>
-        <div className="three columns">
-          <img
-            className="card"
-            alt="alt"
-            src="https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"
-          />
-        </div>
-        <div className="three columns">
-          <img
-            className="card"
-            alt="alt"
-            src="https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"
-          />
-        </div>
-        <div className="three columns">
-          <img
-            className="card"
-            alt="alt"
-            src="https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"
-          />
-        </div>
+        <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
+        <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
+        <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
+        <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
       </div>
       <div className="row">
-        <div className="three columns">
-          <img
-            className="card"
-            alt="alt"
-            src="https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"
-          />
-        </div>
-        <div className="three columns">
-          <img
-            className="card"
-            alt="alt"
-            src="https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"
-          />
-        </div>
-        <div className="three columns">
-          <img
-            className="card"
-            alt="alt"
-            src="https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"
-          />
-        </div>
-        <div className="three columns">
-          <img
-            className="card"
-            alt="alt"
-            src="https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"
-          />
-        </div>
+        <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
+        <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
+        <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
+        <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
       </div>
     </div>
   </div>
---
+++ src/Card/Card.js
@@ -0,0 +1,11 @@
+const Card = (props) => {
+  return (
+    <div className="three columns">
+          <img className="card" alt="alt" src={props.src} />
+          
+            
+      </div>
+  );
+};
+
+export default Card;
