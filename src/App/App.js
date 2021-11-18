707dc32697473c7e2ce69b43039ab5ee52c6123b
VictorLucenna <andvtor@gmail.com>
2021-11-18 00:29:07 -0300

    Colocando imagens da lista nos cards

--- src/App/App.js
+++ src/App/App.js
@@ -9,16 +9,16 @@ const App = () => {
   "https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000013.png",
   "https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000014.png",
   "https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000015.png",
-  "https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000016.png",
-  "https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000017.png",
+  "https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000020.png",
+  "https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000021.png",
   "https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000010.png",
   "https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000011.png",
   "https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000012.png",
   "https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000013.png",
   "https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000014.png",
   "https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000015.png",
-  "https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000016.png",
-  "https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000017.png"];
+  "https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000020.png",
+  "https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000021.png"];
 
   return (
     <div className="App">
@@ -27,28 +27,28 @@ const App = () => {
       </div>
       <div className="container">
         <div className="row">
-          <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
-          <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
-          <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
-          <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
+          <Card src={cardlist[0]} />
+          <Card src={cardlist[1]} />
+          <Card src={cardlist[2]} />
+          <Card src={cardlist[3]} />
         </div>
         <div className="row">
-          <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
-          <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
-          <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
-          <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
+          <Card src={cardlist[4]} />
+          <Card src={cardlist[5]} />
+          <Card src={cardlist[6]} />
+          <Card src={cardlist[7]} />
         </div>
         <div className="row">
-          <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
-          <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
-          <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
-          <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
+          <Card src={cardlist[7]} />
+          <Card src={cardlist[6]} />
+          <Card src={cardlist[5]} />
+          <Card src={cardlist[4]} />
         </div>
         <div className="row">
-          <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
-          <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
-          <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
-          <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
+          <Card src={cardlist[3]} />
+          <Card src={cardlist[2]} />
+          <Card src={cardlist[1]} />
+          <Card src={cardlist[0]} />
         </div>
       </div>
     </div>
