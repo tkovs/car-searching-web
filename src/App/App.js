c8580e5cf8a89d5338b092a2e508427f8e54da54
VictorLucenna <andvtor@gmail.com>
2021-11-18 00:25:35 -0300

    Edit: Array dentro do App

--- src/App/App.js
+++ src/App/App.js
@@ -1,56 +1,58 @@
 import Card from "../Card";
 import "../styles.css";
 
-const App = () => (
+const App = () => {
   
-  <div className="App">
-    <div className="container">
-      <h1 id="title">Jodo da Memoria</h1>
-    </div>
-    <div className="container">
-      <div className="row">
-        <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
-        <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
-        <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
-        <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
-      </div>
-      <div className="row">
-        <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
-        <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
-        <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
-        <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
-      </div>
-      <div className="row">
-        <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
-        <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
-        <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
-        <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
-      </div>
-      <div className="row">
-        <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
-        <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
-        <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
-        <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
-      </div>
-    </div>
-  </div>
-);
+  const cardlist = ["https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000010.png",
+  "https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000011.png",
+  "https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000012.png",
+  "https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000013.png",
+  "https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000014.png",
+  "https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000015.png",
+  "https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000016.png",
+  "https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000017.png",
+  "https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000010.png",
+  "https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000011.png",
+  "https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000012.png",
+  "https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000013.png",
+  "https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000014.png",
+  "https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000015.png",
+  "https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000016.png",
+  "https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000017.png"];
 
-const cardlist = ["https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000010.png",
-"https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000011.png",
-"https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000012.png",
-"https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000013.png",
-"https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000014.png",
-"https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000015.png",
-"https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000016.png",
-"https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000017.png",
-"https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000010.png",
-"https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000011.png",
-"https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000012.png",
-"https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000013.png",
-"https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000014.png",
-"https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000015.png",
-"https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000016.png",
-"https://raw.githubusercontent.com/Perham94/YGOAnimeMangaHQPics/master/100000017.png"];
+  return (
+    <div className="App">
+      <div className="container">
+        <h1 id="title">Jodo da Memoria</h1>
+      </div>
+      <div className="container">
+        <div className="row">
+          <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
+          <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
+          <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
+          <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
+        </div>
+        <div className="row">
+          <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
+          <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
+          <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
+          <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
+        </div>
+        <div className="row">
+          <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
+          <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
+          <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
+          <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
+        </div>
+        <div className="row">
+          <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
+          <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
+          <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
+          <Card src={"https://artprojectsforkids.org/wp-content/uploads/2021/01/Rubber-Ducky.jpeg"} />
+        </div>
+      </div>
+    </div>
+  )
+};
 
 export default App;
