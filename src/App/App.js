216cde1c26c06e900a1fb20bac3eb78cb1d934e0
tkovs <joao.rodrigues@arapiraca.ufal.br>
2022-01-03 18:43:55 -0300

    Alteração x"

--- src/App/App.js
+++ src/App/App.js
@@ -1,18 +1,24 @@
-import Card from "../Card";
-import "../styles.css";
-import { useState } from 'react';
+import Card from '../Card'
+import '../styles.css'
+import { useState } from 'react'
 
 const generateCardList = () => {
-  const base = [46986414, 89631139, 55144522, 83764719, 48372950, 51786039, 97316367, 49094491, 66290900, 86962245, 81344637, 79905468, 87871125, 52945066, 93084621, 44163252, 52097679, 40619741, 33655493, 44046281, 36361633, 43096270, 88975532, 79852326, 77622396, 10000020, 37576645, 10000010, 10000000, 70781052, 15025844, 4031928, 80666118, 42941100]
-  
+  const base = [
+    46986414, 89631139, 55144522, 83764719, 48372950, 51786039, 97316367,
+    49094491, 66290900, 86962245, 81344637, 79905468, 87871125, 52945066,
+    93084621, 44163252, 52097679, 40619741, 33655493, 44046281, 36361633,
+    43096270, 88975532, 79852326, 77622396, 10000020, 37576645, 10000010,
+    10000000, 70781052, 15025844, 4031928, 80666118, 42941100,
+  ]
+
   base.sort(() => 0.5 - Math.random())
   const cardlist = [...base.slice(0, 8), ...base.slice(0, 8)]
 
-  return cardlist.sort(() => 0.5 - Math.random());
+  return cardlist.sort(() => 0.5 - Math.random())
 }
 
 const App = () => {
-  const [cardlist, setCardlist] = useState(generateCardList());
+  const [cardlist, setCardlist] = useState(generateCardList())
 
   const handleRestart = () => {
     setCardlist(generateCardList())
@@ -50,10 +56,9 @@ const App = () => {
         </div>
 
         <button onClick={handleRestart}>Shuffle</button>
-
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
